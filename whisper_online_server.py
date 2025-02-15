1d624440dad28ca9b2391c5061c3d597f3b700a4
Xabi <xezpeleta@gmail.com>
2025-02-15 22:58:32 +0100

    Listen all by default

--- whisper_online_server.py
+++ whisper_online_server.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 parser = argparse.ArgumentParser()
 
 # server options
-parser.add_argument("--host", type=str, default='localhost')
+parser.add_argument("--host", type=str, default='0.0.0.0')
 parser.add_argument("--port", type=int, default=43007)
 parser.add_argument("--warmup-file", type=str, dest="warmup_file", 
         help="The path to a speech audio wav file to warm up Whisper so that the very first chunk processing is fast. It can be e.g. https://github.com/ggerganov/whisper.cpp/raw/master/samples/jfk.wav .")
